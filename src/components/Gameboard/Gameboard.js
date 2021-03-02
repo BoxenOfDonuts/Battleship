0af9c990da99b02ef41e42d4c2f91c354c91925c
Joel H <joel.hacke@gmail.com>
2021-03-01 20:58:05 -0600

    Gameboard logic and tests

--- src/components/Gameboard/Gameboard.js
+++ src/components/Gameboard/Gameboard.js
@@ -2,14 +2,48 @@ import Ship from '../Ship/Ship'
 
 const Gameboard = () => {
   const board = Array(72).fill(null);
+  const inventory = []
+  let areAllSunk = false;
+  let sunkCount = 0;
+
+  const _shipSunk = () => {
+    sunkCount++;
+    if (inventory.length === sunkCount) {
+      areAllSunk = true;
+    }
+  }
 
   const placeShip = (coordinate, length, isVertical) => {
     const ship = Ship(length);
     if (!isVertical) ship.flipOrientation();
-    
+    for (let i = coordinate; i < coordinate + length; i++) {
+      board.splice(i, 1, ship)
+    }
+    inventory.push(ship)
   }
 
-  return { placeShip }
+  const recieveAttack = (coordinate) => {
+    if (board[coordinate] !== null) {
+      const ship = board[coordinate];
+      ship.hit(0);
+      if (ship.isSunk()) {
+        _shipSunk();
+      }
+    } else {
+      board[coordinate] = false;
+    }
+
+  }
+ 
+  const theBoard = () => {
+    return board;
+  }
+
+  const sink = () => {
+    return areAllSunk;
+  }
+
+  return { placeShip, theBoard, recieveAttack, sink }
 
 }
 
--- src/components/Gameboard/Gameboard.test.js
+++ src/components/Gameboard/Gameboard.test.js
@@ -1,6 +1,47 @@
 import Gameboard from './Gameboard';
 
 describe('Test Suite for Gameboard', () => {
-  it('', () =>{
+  it('places a ship', () => {
+    const gameboard = Gameboard();
+    gameboard.placeShip(7, 3, false);
+    expect(gameboard.theBoard()[6]).toBeNull();
+    expect(gameboard.theBoard()[10]).toBeNull();
+    expect(gameboard.theBoard()[7]).not.toBeNull();
+    expect(gameboard.theBoard()[8]).not.toBeNull();
+    expect(gameboard.theBoard()[9]).not.toBeNull();
+  })
+  it('placing a ship does not increase the size of the array', () => {
+    const gameboard = Gameboard();
+    gameboard.placeShip(7, 3, false);
+    expect(gameboard.theBoard().length).toBe(72);
+  })
+  it('ship takes a hit', () => {
+    const gameboard = Gameboard();
+    gameboard.placeShip(7, 3, false);
+    gameboard.recieveAttack(7);
+    expect(gameboard.theBoard()[7].getHull()).toEqual([true, false, false])
+    expect(gameboard.theBoard()[8].getHull()).toEqual([true, false, false])
+    expect(gameboard.theBoard()[9].getHull()).toEqual([true, false, false])
+  })
+  it('misses are tracked', () => {
+    const gameboard = Gameboard();
+    gameboard.recieveAttack(7);
+    expect(gameboard.theBoard()[7]).toBe(false)
+    expect(gameboard.theBoard()[7]).not.toBeNull()
+    expect(gameboard.theBoard().length).toBe(72);
+  })
+  it('creates multiple unique ships', () => {
+    const gameboard = Gameboard();
+    gameboard.placeShip(7, 3, false);
+    gameboard.placeShip(0, 1, false);
+    gameboard.recieveAttack(0);
+    expect(gameboard.theBoard()[0].getHull()).toEqual([true])
+    expect(gameboard.theBoard()[7].getHull()).toEqual([false, false, false])
+  })
+  it('sinking the last ship updates areAllSunk', () => {
+    const gameboard = Gameboard();
+    gameboard.placeShip(0, 1, false);
+    gameboard.recieveAttack(0);
+    expect(gameboard.sink()).toBe(true)
   })
 })
