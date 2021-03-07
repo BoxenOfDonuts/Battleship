003c4687ff7b22b92638f9b8fb3db7940e000c81
Joel H <joel.hacke@gmail.com>
2021-03-07 12:52:58 -0600

    added ship types

---
+++ src/factories/Ship/ShipTypes.js
@@ -0,0 +1,11 @@
+import Ship from "./Ship"
+
+const ShipTypes = {
+  'Carrier': {length: 5, numberOf: 1},
+  'Battleship': {length: 5, numberOf: 2},
+  'Destroyer': {length: 5, numberOf: 3},
+  'Submarine': {length: 5, numberOf: 4},
+  'Patrol Boat': {length: 2, numberOf: 5},
+}
+
+export default ShipTypes
